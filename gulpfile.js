9515097cecfb6bc7c48df5a62fab114c378018ca
SupinePandora43 <xbox.account@bk.ru>
2019-06-18 23:28:23 +0500

    some tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require("gulp")
 var jsonlint = require("gulp-jsonlint")
-
+var zip = require("gulp-zip")
+// RegExp(/[.]\/assets\/**\/*.json/gim)
 gulp.task("json", async () => {
     await gulp.src("./assets/**/*.json")
         .pipe(jsonlint())
         .pipe(jsonlint.failOnError())
         .pipe(jsonlint.reporter())
 })
-exports.default = gulp.series("json")
+gulp.task("zip", async () => {
+    await gulp.src(["**/assets/**", "pack.*"])
+        .pipe(zip("rails-3d.zip",{compress:false}))
+        .pipe(gulp.dest("dist"))
+})
+exports.default = gulp.series("json", "zip")
