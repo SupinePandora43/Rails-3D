e5ee749c20651fcaf78a8953f9bc6b8b9a5c41e5
SupinePandora43 <supinepandoraggg@gmail.com>
2019-07-10 15:14:09 +0500

    some edits

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
-var gulp = require("gulp")
-var jsonlint = require("gulp-jsonlint")
-var zip = require("gulp-zip")
-// RegExp(/[.]\/assets\/**\/*.json/gim)
-gulp.task("json", async () => {
-	await gulp.src(["./assets/**/*.json", "pack.mcmeta"])
+const { src, task, series } = require("gulp")
+const jsonlint = require("gulp-jsonlint")
+const zip = require("gulp-zip")
+
+task("json", async () => {
+	await src(["./assets/**/*.json", "pack.mcmeta"])
 		.pipe(jsonlint())
 		.pipe(jsonlint.failOnError())
 		.pipe(jsonlint.reporter())
 })
-gulp.task("zip", async () => {
-	await gulp.src(["**/assets/**", "pack.*"])
+
+task("zip", async () => {
+	await src(["**/assets/**", "pack.*"])
 		.pipe(zip("rails-3d.zip", { compress: false }))
 		.pipe(gulp.dest("."))
 })
-exports.default = gulp.series("json", "zip")
+
+task("upload", async () => {
+	// ToDo
+})
+
+exports.default = series("json", "zip")
